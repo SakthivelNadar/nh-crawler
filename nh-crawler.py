5bdda6aa6e2ad45a47144c0401673b433daba3e7
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2022-01-11 00:35:50 +0530

    nh-crawler: refactor pdf download logic
    Co-authored-by: AtomicXZ <achubadyal4@gmail.com>
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- nh-crawler.py
+++ nh-crawler.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 from requests import get
-import urllib.request
-import os
-import img2pdf
 from PIL import Image
+import urllib.request
+import shutil
+import os
+
 
 uinput = input("Enter NH code / link:  ")
 
@@ -17,18 +18,21 @@ soup = BeautifulSoup(page, "html.parser")
 title = soup.find("div", {"id" : "info"}).h1.text
 totalPages = len(soup.find_all("div", {"class" : "thumb-container"}))
 
-print(f"Downloading {title}")
+print(f"\nDownloading {title}")
 dir = f"Downloads/{title}"
 os.makedirs(dir)
-pdf_path = "Downloads/nh-converter"
-pdf_bytes = img2pdf.convert(dir)
-file = open(pdf_path, "wb")
-file.write(pdf_bytes)
-dir.close()
-file.close()
 
 for i in range(1, totalPages+1):
     imgData = get(f"{link}/{i}").text
     parsed = BeautifulSoup(imgData, "html.parser")
     imgDdl = parsed.find("section", {"id" : "image-container"}).img["src"]
     urllib.request.urlretrieve(imgDdl, f"{dir}/{i}")
+
+print("\nCreating PDF. Please wait")
+imgList = []
+for i in range(1, totalPages+1):
+    imgList.append(Image.open(f"{dir}/{i}"))
+imgList[0].save(f"Downloads/{title}.pdf", "PDF", resolution = 100.0, save_all = True, append_images = imgList[1:])
+
+print("\nCleaning up.")
+shutil.rmtree(dir)
